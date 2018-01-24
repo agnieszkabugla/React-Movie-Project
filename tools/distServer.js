c6eed362789db704d7861049878041a9272065ba
agnieszkabugla <agnieszka.bugla3@gmail.com>
2018-01-24 11:25:23 +0100

    migrating to webpack 3 and changing webpack.config.dev.js

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import path from 'path';
+import compression from 'compression'; 
 
 /* eslint-disable no-console */
 
 const port = 8080;
 const app = express();
 
+app.use(compression()); 
 app.use(express.static(path.join(__dirname, '../dist')));
 
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,49 +1,52 @@
 import webpack from 'webpack';
 import path from 'path';
 
-export default {
-  debug: true,
-  devtool: 'inline-source-map',
-  noInfo: false,
-  entry: [
-    //'eventsource-polyfill', //necessary with hot reloading in IE
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails
-    path.resolve(__dirname, './public/src/index')
-  ],
+const config = {
+  context: path.resolve(__dirname, './public/src/'),
+  entry: {
+    // removing 'public/src' directory from entry point, since 'context' is taking care of that
+    app: './index.js'
+  },
   target: 'web',
   output: {
-    path:path.resolve(__dirname, 'dist'),  //note: physical files are only output by the production build task 'npm run build'
+    path: path.resolve(__dirname, 'dist'),  //note: physical files are only output by the production build task 'npm run build'
     publicPath: '/',
     filename: 'bundle.js'
   },
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, './public/src')
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
-    loaders: [
-      { test: /\.js$/, include: path.join(__dirname, './public/src'), exclude: 'node_modules', loaders: ['babel'] },
-      { test: /\.css$/, loaders: ['style', 'css'] },
-      { test: /\.jpeg$/, loader: "url-loader" }, 
-      { test: /\.png$/, loader: "url-loader" }, 
-
-      /* last four loaders are for Bootstrap's fonts */
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
-    ],
-    query: {
-      presets: ['react', 'es2015']
-    }
-  },
-    resolve: {
-    extensions: ['', '.js', '.jsx']
+    rules: [
+      { 
+        test: /\.js$/, 
+        include: path.join(__dirname, './public/src/'), 
+        exclude: path.join(__dirname, './node_modules/'), 
+        use: {loader: 'babel-loader'} 
+      },
+      { 
+        test: /\.css$/, 
+        use: [{loader: 'style'}, {loader: 'css'}]          
+      },
+      { 
+        test: /\.jpeg$/,
+        use:{loader: "url-loader"}  
+      }, 
+      { 
+        test: /\.png$/, 
+        use: {loader: "url-loader"} 
+      }
+    ]
   }
+    
 };
+
+module.exports = config; 
 
 /* module.exports = {
   entry: [
