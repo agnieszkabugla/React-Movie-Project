412d6ba036d32a6d28ec982c12c636eac89566c6
agnieszkabugla <agnieszka.bugla3@gmail.com>
2018-01-24 11:50:51 +0100

    changing webpack.config.prod.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -47,31 +47,3 @@ const config = {
 };
 
 module.exports = config; 
-
-/* module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: 'public',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
-}; */
-
